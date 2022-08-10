265e9fb013344ecfca34cd013d05f875448a6d2d
armycrih <armycrih@gmail.com>
2022-08-10 13:10:25 -0300

    config .env

--- config/db.py
+++ config/db.py
@@ -1,10 +1,22 @@
+import os
 # SQLAlchemy
 from sqlalchemy import create_engine
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:toor@localhost:3306/iomanoid"
+from settings import Settings
+
+settings = Settings()
+
+DB_NAME = settings.db_name
+DB_USER = settings.db_user
+DB_PASS = settings.db_pass
+DB_HOST = settings.db_host
+DB_PORT = settings.db_port
+
+
+DATABASE_URL = "mysql+pymysql://"+DB_USER+":"+DB_PASS+"@"+DB_HOST+":"+DB_PORT+"/"+DB_NAME
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
