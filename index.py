f6910c1242a77ad386e8e008a6b68af867b6366d
armycrih <armycrih@gmail.com>
2022-08-13 22:19:53 -0300

    redirection on main page to docs

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
-
+from starlette.responses import RedirectResponse
 #from routes import user
 #from routes.project import project
 from routes import user
 
 app = FastAPI()
+
+@app.get("/")
+def main():
+    return RedirectResponse(url="/docs/")
 
 app.include_router(user)
 #app.include_router(project)
